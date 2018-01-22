c1d3af27e6b64ad7bfe29b010529452f39dd763b
SciTechDude <anil.mandra@yahoo.com>
2018-01-21 23:40:05 -0600

    #rank displayed inside bubbles along-with tag name

--- sostats.py
+++ sostats.py
@@ -2,24 +2,28 @@ import stackexchange
 import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import matplotlib.cm as cm
+#from matplotlib import rcParams
+
 import numpy as np
 import math
 import seaborn as sns
 
-so = stackexchange.Site(stackexchange.StackOverflow)
-
 # Set default Seaborn style
 sns.set()
 
-#Print top 10 tags and their count
+#Connect to SO site
+so = stackexchange.Site(stackexchange.StackOverflow)
+
+#Initialize lists to hold  values
 tag_num = 10
 tag_name = list()
 tag_count = list()
 tag_rank = list()
 
+#get required values from SO object
 for  idx, tag in enumerate(so.tags()):
-    if idx >= 10:
-        break
+    if idx >= tag_num:
+        break    
     tag_rank.append(idx+1)
     tag_name.append(tag.name)
     tag_count.append(tag.count)
@@ -28,7 +32,7 @@ for  idx, tag in enumerate(so.tags()):
 colors=cm.rainbow(np.random.rand(2 * tag_num))
 
 #Set bubble size
-bubble_size = [(float(i) / tag_count[0])*1000  for i in tag_count]
+bubble_size = [int(i / np.std(tag_count)*500) for i in tag_count]
 
 
 def millions(x, pos):
@@ -42,23 +46,29 @@ formatter = FuncFormatter(millions)
 
 #create scatter plot
 
-#fig, ax = plt.subplots()
-ax = plt.gca()
+fig, ax = plt.subplots()
 ax.yaxis.set_major_formatter(formatter)
 ax.scatter(tag_rank,tag_count,s=bubble_size,marker='o', color=colors)
-#ax.scatter(tag_rank,tag_count,s=bubble_size,marker='o', c=bubble_size )
-#ax.scatter(tag_rank,tag_count,s=bubble_size, color=colors)
 
 #label each bubble
-for i in range(tag_num):
-    plt.annotate(tag_name[i],xy=(tag_rank[i], tag_count[i]))
+for n,c,r,s in zip(tag_name,tag_count,tag_rank,bubble_size):
+    plt.annotate("#{}".format(r),xy=(r, c), ha="center", va="center")
+    plt.annotate(n ,xy=(r, c), xytext=(0,np.sqrt(s)/2.+5), 
+                textcoords="offset points", ha="center", va="bottom")
+
+#increase x, y axis limit 10% more
+ymin = ax.get_ylim()[0]
+ymax = ax.get_ylim()[1]
+plt.ylim(ymin, 1.10 * ymax)
+
+# Setfont dictionaries for plot title and axis titles
+title_font = {'fontname':'Arial', 'size':'16', 'color':'black', 'weight':'normal',
+              'verticalalignment':'bottom'} 
+axis_font = {'fontname':'Arial', 'size':'14'}
 
 
 #Label axis
-#plt.tight_layout()
-plt.rcParams['xtick.major.pad'] = 8
-plt.rcParams['ytick.major.pad'] = 8
-plt.xlabel('Tag Count')
-plt.ylabel(' Tag Rank')
-plt.title('Top Ten Tags By Counts',y=1.05)
+plt.ylabel('Tag Count', **axis_font)
+plt.xlabel('Tag Rank', **axis_font)
+plt.title('Top Ten Tags By Counts', **title_font)
 plt.show()
